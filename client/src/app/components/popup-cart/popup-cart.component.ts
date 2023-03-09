22a6a74d54333573cbd08ee9d49dc513305f37b7
justsaykk <kaikein.woo@gmail.com>
2023-03-09 14:24:42 +0800

    Added function to increment popup item

--- client/src/app/components/popup-cart/popup-cart.component.ts
+++ client/src/app/components/popup-cart/popup-cart.component.ts
@@ -9,13 +9,13 @@ import { ShoppingcartService } from 'src/app/services/shoppingcart.service';
   styleUrls: ['./popup-cart.component.css']
 })
 export class PopupCartComponent implements OnInit, OnDestroy {
-
   constructor(
     private cartSvc: ShoppingcartService, 
   ) { }
 
   shoppingCart$!: Subscription;
   shoppingCart!: Drink[]
+  cartItems: CartItem[] = []
   title: string = "Orders"
 
   ngOnInit(): void {
@@ -24,10 +24,36 @@ export class PopupCartComponent implements OnInit, OnDestroy {
         this.shoppingCart = cart;
       }
     )
+      this.toCartItem(this.shoppingCart);
   }
 
   ngOnDestroy(): void {
       this.shoppingCart$.unsubscribe();
   }
 
+  private toCartItem(_shoppingCart: Drink[]) {
+    console.log("toCartItem start");
+    
+    if (!_shoppingCart.length) {
+      console.log("Shopping cart is empty");
+      return
+    }
+
+    _shoppingCart.map(
+      (_drink: Drink) => {
+        let index = this.cartItems.findIndex(el => _drink.idDrink === el.drink.idDrink);
+        if ( !this.cartItems.length || index < 0 )
+        { this.cartItems.push({drink: _drink, quantity: 1} as CartItem)
+          return 
+        }
+        this.cartItems[index].quantity += 1;
+        return
+      }
+    )
+  }
+}
+
+export interface CartItem {
+  drink: Drink
+  quantity: number
 }
