8ff1b093302f99622512d6238da2096fa7b8d916
justsaykk <kaikein.woo@gmail.com>
2023-04-18 20:47:21 +0800

    Used byteArrayis to pass env variable

--- server/src/main/java/com/vttpfinalproject/backend/config/FirebaseConfig.java
+++ server/src/main/java/com/vttpfinalproject/backend/config/FirebaseConfig.java
@@ -3,6 +3,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import org.springframework.context.annotation.Bean;
@@ -13,9 +14,12 @@ public class FirebaseConfig {
 
     @Bean
     public FirebaseApp InitializeFireBaseApp() throws IOException {
+
+        String firebaseConfig = System.getenv("FIREBASE_CONFIG");
+
         FirebaseOptions fbOptions = FirebaseOptions.builder()
         // Credentials supplied via Environment Variables
-        .setCredentials(GoogleCredentials.getApplicationDefault())
+        .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(firebaseConfig.getBytes())))
         .build();
 
         return FirebaseApp.initializeApp(fbOptions);
