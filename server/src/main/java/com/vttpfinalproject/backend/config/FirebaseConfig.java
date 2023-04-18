b665a85f265e1d944e6a4bec04b6d944ffa28b7d
justsaykk <kaikein.woo@gmail.com>
2023-04-18 20:27:38 +0800

    Changed FirebaseConfig file and set google credentials to get via env variables

--- server/src/main/java/com/vttpfinalproject/backend/config/FirebaseConfig.java
+++ server/src/main/java/com/vttpfinalproject/backend/config/FirebaseConfig.java
@@ -5,23 +5,20 @@ import com.google.firebase.FirebaseOptions;
 
 import java.io.IOException;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FirebaseConfig {
 
-
-    private FirebaseOptions buildFireBaseOptions() throws IOException {
-        // Build the FirebaseOptions object using the decoded JSON object
-        return FirebaseOptions.builder()
+    @Bean
+    public FirebaseApp InitializeFireBaseApp() throws IOException {
+        FirebaseOptions fbOptions = FirebaseOptions.builder()
+        // Credentials supplied via Environment Variables
         .setCredentials(GoogleCredentials.getApplicationDefault())
-        .setProjectId("drink-factory-281e7")
-        .setStorageBucket("drink-factory-281e7.appspot.com")
         .build();
-    }
 
-    public void InitializeFireBaseApp() throws IOException {
-        FirebaseApp.initializeApp(this.buildFireBaseOptions());
+        return FirebaseApp.initializeApp(fbOptions);
     }
 
 }
