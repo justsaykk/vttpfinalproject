b5e76d251ec1e2e7c1e6a1ade3bdd8dfbed5b3ce
justsaykk <kaikein.woo@gmail.com>
2023-04-21 14:29:22 +0800

    Troubleshooting why createuser is called for profile update

--- server/src/main/java/com/vttpfinalproject/backend/controllers/ProfileController.java
+++ server/src/main/java/com/vttpfinalproject/backend/controllers/ProfileController.java
@@ -63,8 +63,10 @@ public class ProfileController {
         }
 
         FirebaseToken fbToken = optFbToken.get();
+        System.out.println(fbToken.getUid());
         Optional<User> dbUserOptional = userSvc.getUserByUID(fbToken.getUid());
         if (dbUserOptional.isEmpty()) {
+            System.out.println("No Such Firebase user. Creating New user");
             userSvc.createUser(new User(fbToken));
             dbUserOptional = userSvc.getUserByUID(fbToken.getUid());
         }
