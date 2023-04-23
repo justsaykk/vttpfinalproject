382954fa26fa6c585cd2014f576f877400d55036
justsaykk <kaikein.woo@gmail.com>
2023-04-23 10:42:54 +0800

    Added codes to get drink details by ID

--- server/src/main/java/com/vttpfinalproject/backend/controllers/RESTController.java
+++ server/src/main/java/com/vttpfinalproject/backend/controllers/RESTController.java
@@ -3,17 +3,17 @@ package com.vttpfinalproject.backend.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vttpfinalproject.backend.models.DetailedDrink;
 import com.vttpfinalproject.backend.models.Drink;
 import com.vttpfinalproject.backend.services.DrinkService;
 
@@ -27,10 +27,6 @@ public class RESTController {
 
     @Autowired
     private DrinkService drinkSvc;
-
-
-    @Value("${spring.stripe.endpointsecret}")
-    private String stripeSecret;
     
     @GetMapping(path = "/menu")
     public ResponseEntity<String> getMenu(
@@ -44,4 +40,13 @@ public class RESTController {
             Json.createObjectBuilder().add("result", jab).build().toString()
             , HttpStatus.OK);
     }
+
+    @GetMapping(path = "/drink/{id}")
+    public ResponseEntity<String> getDrinkById(
+        @PathVariable(name = "id") String drinkId
+    ) {
+        DetailedDrink detailedDrink = drinkSvc.fetchDrinkById(drinkId);
+        return new ResponseEntity<String>(detailedDrink.toJson().toString(), HttpStatus.OK);
+    }
+
 }
