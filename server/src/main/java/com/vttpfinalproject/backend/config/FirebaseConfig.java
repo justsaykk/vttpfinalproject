42ee0ceef9e740b3804a6f79e594a266b37cb33e
justsaykk <kaikein.woo@gmail.com>
2023-05-11 14:19:47 +0800

    Changed firebase config to revert to original

--- server/src/main/java/com/vttpfinalproject/backend/config/FirebaseConfig.java
+++ server/src/main/java/com/vttpfinalproject/backend/config/FirebaseConfig.java
@@ -1,10 +1,10 @@
 package com.vttpfinalproject.backend.config;
 
-// import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
-// import com.google.firebase.FirebaseOptions;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseOptions;
 
-// import java.io.ByteArrayInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import org.springframework.context.annotation.Bean;
@@ -15,12 +15,11 @@ public class FirebaseConfig {
 
     @Bean
     public FirebaseApp InitializeFireBaseApp() throws IOException {
-        // // When deployed on railway
-        // String firebaseConfig = System.getenv("FIREBASE_CONFIG");
-        // FirebaseOptions fbOptions = FirebaseOptions.builder()
-        // .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(firebaseConfig.getBytes())))
-        // .build();
-        return FirebaseApp.initializeApp();
+        String firebaseConfig = System.getenv("FIREBASE_CONFIG");
+        FirebaseOptions fbOptions = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(firebaseConfig.getBytes())))
+        .build();
+        return FirebaseApp.initializeApp(fbOptions);
     }
 
 }
