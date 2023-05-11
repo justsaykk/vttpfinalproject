16d57580dd5f78a6bebdc425a19154b0557629d6
justsaykk <kaikein.woo@gmail.com>
2023-05-11 14:12:21 +0800

    Changed config codes

--- server/src/main/java/com/vttpfinalproject/backend/config/FirebaseConfig.java
+++ server/src/main/java/com/vttpfinalproject/backend/config/FirebaseConfig.java
@@ -1,7 +1,8 @@
 package com.vttpfinalproject.backend.config;
-import com.google.auth.oauth2.GoogleCredentials;
+
+// import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
+// import com.google.firebase.FirebaseOptions;
 
 // import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -14,18 +15,12 @@ public class FirebaseConfig {
 
     @Bean
     public FirebaseApp InitializeFireBaseApp() throws IOException {
-        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
-        FirebaseOptions options = FirebaseOptions.builder()
-        .setCredentials(credentials)
-        .setProjectId("drink-factory-281e7")
-        .build();
-
         // // When deployed on railway
         // String firebaseConfig = System.getenv("FIREBASE_CONFIG");
         // FirebaseOptions fbOptions = FirebaseOptions.builder()
         // .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(firebaseConfig.getBytes())))
         // .build();
-        return FirebaseApp.initializeApp(options);
+        return FirebaseApp.initializeApp();
     }
 
 }
--- server/src/main/java/com/vttpfinalproject/backend/repository/SqlQueries.java
+++ server/src/main/java/com/vttpfinalproject/backend/repository/SqlQueries.java
@@ -1,21 +1,21 @@
-package com.vttpfinalproject.backend.repository;
+// package com.vttpfinalproject.backend.repository;
 
-public class SqlQueries {
-//     public static final String SQL_GET_TRANSACTION_BY_ID = "SELECT * FROM transactions";
-    public static final String SQL_INSERT_TRANSACTION = """
-            INSERT INTO transactions (session_id, amount_total) 
-            VALUES (?, ?)
-            """;
-    public static final String SQL_GET_USERS_BY_EMAIL = "SELECT * FROM users WHERE email = ?";
-    public static final String SQL_GET_USERS_BY_UID = "SELECT * FROM users WHERE firebaseUID = ?";
-    public static final String SQL_CREATE_USER = """
-            INSERT INTO users (email, name, profilePic, firebaseUID)
-            VALUES (?, ?, ?, ?)
-            """;
-    public static final String SQL_UPDATE_USER = """
-            UPDATE users
-            SET name = ?, profilePic = ?
-            WHERE firebaseUID = ?
-            """;
-    public static final String SQL_DELETE_USER = "DELETE FROM users WHERE firebaseUID = ?";
-}
+// public class SqlQueries {
+// //     public static final String SQL_GET_TRANSACTION_BY_ID = "SELECT * FROM transactions";
+//     public static final String SQL_INSERT_TRANSACTION = """
+//             INSERT INTO transactions (session_id, amount_total) 
+//             VALUES (?, ?)
+//             """;
+//     public static final String SQL_GET_USERS_BY_EMAIL = "SELECT * FROM users WHERE email = ?";
+//     public static final String SQL_GET_USERS_BY_UID = "SELECT * FROM users WHERE firebaseUID = ?";
+//     public static final String SQL_CREATE_USER = """
+//             INSERT INTO users (email, name, profilePic, firebaseUID)
+//             VALUES (?, ?, ?, ?)
+//             """;
+//     public static final String SQL_UPDATE_USER = """
+//             UPDATE users
+//             SET name = ?, profilePic = ?
+//             WHERE firebaseUID = ?
+//             """;
+//     public static final String SQL_DELETE_USER = "DELETE FROM users WHERE firebaseUID = ?";
+// }
